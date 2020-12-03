ba578374fff3f81385a3db73b093447152e3c223
Jakob <e11809618@student.tuwien.ac.at>
2020-12-03 10:18:16 +0100

    update

--- I-dont-care-about-your-kids/content.js
+++ I-dont-care-about-your-kids/content.js
@@ -1,25 +1,39 @@
 console.log("content.js is running...");
 
+var recipe_identifier = {
+    "tag_names": ["ul"],
+    "class_names": [
+        "wprm-recipe-template-custom",
+        "wprm-recipe-template-mb-custom",
+        "wprm-recipe-template-mb-food" 
+    ]
+}
+
 function scroll(e){
-    e.scrollIntoView()
     e.style.border = "4px solid red"
+    e.scrollIntoView()
 }
 
 var elem = null
-/* simple list elements */
-elem = document.getElementsByTagName("ul")
-/* wordpress recipe maker */
-elem = document.getElementsByClassName("wprm-recipe-template-mb-food")
-elem = document.getElementsByClassName("wprm-recipe-template-custom")
 
+for (var id of recipe_identifier.tag_names){
+    elem = document.getElementsByTagName(id)
+    if (elem.length > 0) break;
+    elem = null
+}
+
+for (var id of recipe_identifier.class_names){
+    elem = document.getElementsByClassName(id)
+    if (elem.length > 0) break;
+    elem = null
+}
 
 if(elem) scroll(elem[0]) 
-
 
 /*
 TODO:
     * click on jump to recipe
     * close newsletter
-    * 
+    * find more elements
 */
 
