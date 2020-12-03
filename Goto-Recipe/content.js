c25f1fa0cb0e361c0f59e053701db9426f0b762f
Jakob <e11809618@student.tuwien.ac.at>
2020-12-03 09:43:24 +0100

    update

--- Goto-Recipe/content.js
+++ Goto-Recipe/content.js
@@ -1,1 +1,25 @@
-alert("Hello content.js");
+console.log("content.js is running...");
+
+function scroll(e){
+    e.scrollIntoView()
+    e.style.border = "4px solid red"
+}
+
+var elem = null
+/* simple list elements */
+elem = document.getElementsByTagName("ul")
+/* wordpress recipe maker */
+elem = document.getElementsByClassName("wprm-recipe-template-mb-food")
+elem = document.getElementsByClassName("wprm-recipe-template-custom")
+
+
+if(elem) scroll(elem[0]) 
+
+
+/*
+TODO:
+    * click on jump to recipe
+    * close newsletter
+    * 
+*/
+
--- README.md
+++ README.md
@@ -7,3 +7,10 @@ https://developer.chrome.com/extensions/overview
 
 https://medium.com/coding-in-simple-english/how-to-create-chrome-extension-7dd396e884ef
 
+## Example pages
+
+https://minimalistbaker.com/sweet-potato-chickpea-buddha-bowl/
+https://www.budgetbytes.com/one-pot-creamy-cajun-chicken-pasta/
+
+
+
