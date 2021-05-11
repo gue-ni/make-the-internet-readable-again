2e4384297496b69ed9ed224f507f464ab445b105
Jakob <jakob.g.maier@gmail.com>
2021-05-11 09:11:44 +0200

    little refactoring

--- chrome_extension/content.js
+++ chrome_extension/content.js
@@ -4,64 +4,64 @@
  * find more elements
  */
 
- var recipe_identifier = {
-    "tag_names": ["ul"],
-    "class_names": [
+const recipe_identifier = {
+    tag_names: ["ul"],
+    class_names: [
         "wprm-recipe-template-custom",
         "wprm-recipe-template-mb-custom",
-        "wprm-recipe-template-mb-food"
+        "wprm-recipe-template-mb-food",
     ],
-    "wild_card": [
-        "wprm-recipe-template"
-    ]
-}
+    wild_card: ["wprm-recipe-template"],
+};
 
-function skipToRecipe(){
+function skipToRecipe() {
     for (const a of document.querySelectorAll("a")) {
-        if (a.textContent.includes("Jump to Recipe") || a.textContent.includes("Jump to Recipe")) {
-            a.click()
-            return
-        }
-    }
-
-    function scroll(e){
-        if (e){
-            e[0].style.border = "4px solid red"
-            e[0].scrollIntoView()
-        }
-    }
-
-    var elem = null
-
-    for (var id of recipe_identifier.wild_card){
-        elem = document.querySelectorAll(`[class^='${id}']`)
-        if (elem.length > 0){
-            scroll(elem)
-            return;
-        }
-    }
-    
-    for (var id of recipe_identifier.class_names){
-        elem = document.getElementsByClassName(id)
-        if (elem.length > 0){
-            scroll(elem)
+        if (
+            a.textContent.includes("Jump to Recipe") ||
+            a.textContent.includes("Jump to Recipe")
+        ) {
+            a.click();
             return;
         }
     }
 
-    for (var id of recipe_identifier.tag_names){
-        elem = document.getElementsByTagName(id)
-        if (elem.length > 0){
-            scroll(elem)
+    function scroll(e) {
+        if (e) {
+            e[0].style.border = "4px solid red";
+            e[0].scrollIntoView();
+        }
+    }
+
+    var elem = null;
+
+    for (var id of recipe_identifier.wild_card) {
+        elem = document.querySelectorAll(`[class^='${id}']`);
+        if (elem.length > 0) {
+            scroll(elem);
+            return;
+        }
+    }
+
+    for (var id of recipe_identifier.class_names) {
+        elem = document.getElementsByClassName(id);
+        if (elem.length > 0) {
+            scroll(elem);
+            return;
+        }
+    }
+
+    for (var id of recipe_identifier.tag_names) {
+        elem = document.getElementsByTagName(id);
+        if (elem.length > 0) {
+            scroll(elem);
             return;
         }
     }
 }
 
-chrome.storage.sync.get(['skipToRecipeOption'], function(result) {
+chrome.storage.sync.get(["skipToRecipeOption"], function (result) {
     //console.log('skipToRecipe currently is ' + result.skipToRecipeOption);
-    if (result.skipToRecipeOption){
-        skipToRecipe()
+    if (result.skipToRecipeOption) {
+        skipToRecipe();
     }
 });
-
